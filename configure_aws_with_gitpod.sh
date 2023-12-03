b6c40c7c3f6b7073fb75a0cd61664cb2d926f862
Muhammad Bilal <f190375@cfd.nu.edu.pk>
2023-12-03 02:11:59 +0000

    feat: Update configure_aws_with_gitpod.sh file

--- configure_aws_with_gitpod.sh
+++ configure_aws_with_gitpod.sh
@@ -1,3 +1,4 @@
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+curl -fSsl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -qq awscliv2.zip
+sudo ./aws/install --update
+rm awscliv2.zip
